88a03284cf8468a54591bb8e9cef9c3e2af7a771
spacesfour <spacesfour@proton.me>
2025-08-23 15:30:45 +0000

    add tests

---
+++ app/demo-web-app/src/test/java/com/mahmud/demo_web_app/controller/DemoControllerTest.java
@@ -0,0 +1,25 @@
+package com.mahmud.demo_web_app.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import com.mahmud.demo_web_app.DemoController;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(DemoController.class)
+public class DemoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHelloEndpoint() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("<h1>Hello, World!</h1>"));
+    }
+}
